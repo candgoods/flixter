53b9aa4c380ac96c8b9a75d1caa2fbdd24854f48
Candace <goodsoncj@gmail.com>
2017-10-28 17:33:10 +0000

    lesson enroll valid

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course
+
   def show
   end
 
   private
+
+  def require_enrolled_for_current_course
+    if !(current_user.enrolled_in?(current_lesson.section.course))
+      redirect_to courses_path, alert: 'Must be enrolled in course'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
